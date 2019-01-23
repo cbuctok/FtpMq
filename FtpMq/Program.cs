f99c64b3df4e2cc09ebacf78c40e3e74ea98cbdf
Greg <cbuctok@gmail.com>
2019-01-23 18:14:57 +0100

    Add project files.

---
+++ FtpMq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FtpMq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
